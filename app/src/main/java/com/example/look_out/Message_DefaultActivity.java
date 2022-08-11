f39128bb709bc9d130afc5a73ea096a0716e8396
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2022-08-11 20:21:05 +0900

    feat : 취소 버튼 구현

--- app/src/main/java/com/example/look_out/Message_DefaultActivity.java
+++ app/src/main/java/com/example/look_out/Message_DefaultActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class Message_DefaultActivity extends AppCompatActivity {
 
     private ImageView home;
+    private Button cancelButton;
+    EditText et_setting;
 
 
     @Override
@@ -17,12 +21,23 @@ public class Message_DefaultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_default);
 
+        et_setting = (EditText)findViewById(R.id.et_setting);
+
+        cancelButton = findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Message_DefaultActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
         home = findViewById(R.id.home);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Message_DefaultActivity.this, MainActivity.class);
-                startActivity(intent); //엑티비티 이동
+                startActivity(intent);
             }
         });
 
