f6958cd324a3fbce931834b2755c4b289a546ba0
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2022-08-11 21:59:24 +0900

    #4 feat: 저장 버튼 동적 구현

--- app/src/main/java/com/example/look_out/Message_119Activity.java
+++ app/src/main/java/com/example/look_out/Message_119Activity.java
@@ -3,10 +3,12 @@ package com.example.look_out;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Message_119Activity extends AppCompatActivity {
 
@@ -19,19 +21,16 @@ public class Message_119Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_119);
 
-
-        Intent intent = getIntent();
-        String message = intent.getStringExtra("message");
+        String message = ((Message_DefaultActivity)Message_DefaultActivity.context_main).message;
         et_default = (EditText)findViewById(R.id.et_default);
         et_default.setText(message);
-
 
         home = findViewById(R.id.home);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Message_119Activity.this, MainActivity.class);
-                startActivity(intent); //엑티비티 이동
+                startActivity(intent);
             }
         });
 
