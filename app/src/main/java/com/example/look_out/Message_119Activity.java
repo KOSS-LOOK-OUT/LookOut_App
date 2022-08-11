dbd1d2e21d8311bf3cca5459e1f8dc95838707ce
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2022-08-11 20:33:56 +0900

    feat : intent로 저장 버튼 동적 구현

--- app/src/main/java/com/example/look_out/Message_119Activity.java
+++ app/src/main/java/com/example/look_out/Message_119Activity.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class Message_119Activity extends AppCompatActivity {
 
     private ImageView home;
+    private EditText et_default;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_119);
+
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        et_default = (EditText)findViewById(R.id.et_default);
+        et_default.setText(message);
+
 
         home = findViewById(R.id.home);
         home.setOnClickListener(new View.OnClickListener() {
