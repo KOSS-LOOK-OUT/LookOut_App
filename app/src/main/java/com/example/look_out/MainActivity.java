63f140896df63e6472e388dfcc79d6f042b1926c
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2022-08-15 20:18:56 +0900

    feat: 문자 권한 설정 적용

--- app/src/main/java/com/example/look_out/MainActivity.java
+++ app/src/main/java/com/example/look_out/MainActivity.java
@@ -18,21 +18,22 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView setting;
-    private final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1000;
+    private final int MY_PERMISSIONS_REQUEST = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
-                Toast.makeText(this, "전화 권한이 필요합니다.", Toast.LENGTH_LONG).show();
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
+        int permissionCall = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+        int permissionSMS = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+
+        if (permissionCall == PackageManager.PERMISSION_DENIED || permissionSMS == PackageManager.PERMISSION_DENIED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+                Toast.makeText(this, "전화&문자 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST);
             } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
-                Toast.makeText(this, "전화 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST);
             }
         }
 
@@ -45,16 +46,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CALL_PHONE:
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "앱 실행을 위한 권한이 설정 되었습니다", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(this, "앱 실행을 위한 권한이 취소 되었습니다", Toast.LENGTH_LONG).show();
+        if (requestCode == 1000) {
+            boolean check_result = true;
+            // 모든 퍼미션을 허용했는지 체크
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
                 }
-                break;
+            }
+            // 권한 체크에 동의를 하지 않으면 안드로이드 종료
+            if(check_result == true) {
+                Toast.makeText(this, "앱 실행을 위한 권한이 설정 되었습니다", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(this, "앱 실행을 위한 권한이 취소 되었습니다", Toast.LENGTH_LONG).show();
+                finish();
+            }
         }
     }
 }
