74cf291171cd91262bae05746a8f250e4e59b0e9
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2022-08-16 16:57:45 +0900

    feat: 진동 울리기 개선

--- app/src/main/java/com/example/look_out/Alarm_thiefActivity.java
+++ app/src/main/java/com/example/look_out/Alarm_thiefActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,16 +14,21 @@ public class Alarm_thiefActivity extends AppCompatActivity {
 
     private Button callButton;
     private Button offButton;
+    private Button messageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_thief);
 
+        final Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(new long[]{100,1000,100,500,100,500,100,1000}, 0); // 무한 진동
+
         callButton = (Button) findViewById(R.id.callButton);
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                vibrator.cancel();
                 String phoneNo = "112";
 
                 Context c = view.getContext();
@@ -40,7 +46,18 @@ public class Alarm_thiefActivity extends AppCompatActivity {
         offButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                vibrator.cancel();
                 Intent intent = new Intent(Alarm_thiefActivity.this, MainActivity.class);
+                startActivity(intent); //엑티비티 이동
+            }
+        });
+
+        messageButton = findViewById(R.id.messageButton);
+        messageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vibrator.cancel();
+                Intent intent = new Intent(Alarm_thiefActivity.this, Message_112Activity.class);
                 startActivity(intent); //엑티비티 이동
             }
         });
