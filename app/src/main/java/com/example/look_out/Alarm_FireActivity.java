3e271d4855c63c0f8dccc713bd91114a9d17024f
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2022-08-16 16:55:51 +0900

    feat: 진동 울리기 개선

--- app/src/main/java/com/example/look_out/Alarm_FireActivity.java
+++ app/src/main/java/com/example/look_out/Alarm_FireActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,16 +15,21 @@ public class Alarm_FireActivity extends AppCompatActivity {
 
     private Button callButton;
     private Button offButton;
+    private Button messageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_fire);
 
+        final Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(new long[]{100,1000,100,500,100,500,100,1000}, 0); // 무한 진동
+
         callButton = (Button) findViewById(R.id.callButton);
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                vibrator.cancel();
                 String phoneNo = "119";
 
                 Context c = view.getContext();
@@ -37,10 +43,21 @@ public class Alarm_FireActivity extends AppCompatActivity {
             }
         });
 
+        messageButton = findViewById(R.id.messageButton);
+        messageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vibrator.cancel();
+                Intent intent = new Intent(Alarm_FireActivity.this, Message_119Activity.class);
+                startActivity(intent); //엑티비티 이동
+            }
+        });
+
         offButton = findViewById(R.id.offButton);
         offButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                vibrator.cancel();
                 Intent intent = new Intent(Alarm_FireActivity.this, MainActivity.class);
                 startActivity(intent); //엑티비티 이동
             }
