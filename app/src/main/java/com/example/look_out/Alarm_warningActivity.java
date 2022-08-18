4fcdb1dedaa9ad4df0a38162d0690c8095658c77
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2022-08-18 23:49:25 +0900

    feat: firebase 데이터 받아 연결 구현

--- app/src/main/java/com/example/look_out/Alarm_warningActivity.java
+++ app/src/main/java/com/example/look_out/Alarm_warningActivity.java
@@ -57,6 +57,7 @@ public class Alarm_warningActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 vibrator.cancel();
+                MainActivity.flag = true;
                 Intent intent = new Intent(Alarm_warningActivity.this, Message_119Activity.class);
                 startActivity(intent); //엑티비티 이동
             }
