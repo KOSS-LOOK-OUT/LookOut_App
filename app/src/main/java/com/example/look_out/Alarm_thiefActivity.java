6130ebde83c0a95fca4d9b0cb3c7c06708017107
hummingbbird <pop9814@kookmin.ac.kr>
2022-08-20 19:51:45 +0900

    fixed untracked files

--- app/src/main/java/com/example/look_out/Alarm_thiefActivity.java
+++ app/src/main/java/com/example/look_out/Alarm_thiefActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
