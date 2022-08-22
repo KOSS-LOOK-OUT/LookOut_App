63fee7f0b83282e754fe05ac1ea586a87a4774e3
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2022-08-22 17:59:39 +0900

    feat: 알림창 로그 저장 기능 구현

---
+++ app/src/main/java/com/example/look_out/LogActivity.java
@@ -0,0 +1,40 @@
+package com.example.look_out;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LogActivity extends AppCompatActivity {
+
+    private ListView listView;
+    List<String> al_log = new ArrayList<>();
+    List<String> allog = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+
+        listView = (ListView)findViewById(R.id.listView);
+
+        ArrayAdapter<String> adpater = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, allog);
+        al_log = ((MainActivity)MainActivity.context_main).al_log;
+
+        for(int i =0; i<al_log.size(); i++){
+            allog.add(al_log.get(i));
+        }
+
+        listView.setAdapter(adpater);
+    }
+
+}
