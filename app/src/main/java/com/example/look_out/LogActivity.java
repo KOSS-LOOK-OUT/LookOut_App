4df46add5632ab3f3406823bf781ff5d8e8e6af1
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2022-08-24 15:21:19 +0900

    feat: 초기화 버튼 구현 및 로그 갯수 50개까지 저장

--- app/src/main/java/com/example/look_out/LogActivity.java
+++ app/src/main/java/com/example/look_out/LogActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -18,6 +20,7 @@ public class LogActivity extends AppCompatActivity {
     private ListView listView;
     ArrayList<String> al_log = new ArrayList<>();
     ArrayList<String> allog = new ArrayList<>();
+    private Button resetButton;
 
 
     @Override
@@ -30,8 +33,26 @@ public class LogActivity extends AppCompatActivity {
         ArrayAdapter<String> adpater = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, allog);
         al_log = ((MainActivity)MainActivity.context_main).al_log;
 
-        for(int i = al_log.size() - 1; i>0; i--){
-            allog.add(al_log.get(i));
+        resetButton = (Button)findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                al_log.clear();
+                allog.clear();
+                if(al_log.size() == 0){
+                    allog.add("보여줄 로그가 없습니다.");
+                }
+                adpater.notifyDataSetChanged();
+            }
+        });
+
+        if(al_log.size() == 0){
+            allog.add("보여줄 로그가 없습니다.");
+        }
+        else {
+            for (int i = al_log.size() - 1; i > al_log.size() - 51; i--) {
+                allog.add(al_log.get(i));
+            }
         }
 
         listView.setAdapter(adpater);
