79c602cbb78bc23f3f6a6253cfb8286fd91a0e68
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2022-08-24 19:42:22 +0900

    feat: 인증번호를 입력하면 device_key를 키 값으로 firebase로 전송 구현

--- app/src/main/java/com/example/look_out/Setting_AddDeviceActivity.java
+++ app/src/main/java/com/example/look_out/Setting_AddDeviceActivity.java
@@ -9,19 +9,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Setting_AddDeviceActivity extends AppCompatActivity {
     private ImageView backButton;
     private Button sendButton;
+    private EditText deviceAddEdit;
+    String device_key;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_device_add);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference();
 
         backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +41,17 @@ public class Setting_AddDeviceActivity extends AppCompatActivity {
             }
         });
 
-        sendButton = findViewById(R.id.sendButton);
+        deviceAddEdit = (EditText) findViewById(R.id.deviceAddEdit);
+
+        sendButton = (Button)findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                device_key = deviceAddEdit.getText().toString();
+                ref.child("device_key").setValue(device_key);
+            }
+        });
+
     }//end of onCreate
 
 }//end of class
