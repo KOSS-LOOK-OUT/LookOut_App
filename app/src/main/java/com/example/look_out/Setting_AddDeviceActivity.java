7381bf476d5aa01eb2824d193849bb9b242c2777
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2022-08-25 16:20:20 +0900

    feat: 디바이스 추가 창에 키 값과 상태 값 업로드

--- app/src/main/java/com/example/look_out/Setting_AddDeviceActivity.java
+++ app/src/main/java/com/example/look_out/Setting_AddDeviceActivity.java
@@ -2,8 +2,11 @@ package com.example.look_out;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +17,9 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.util.ArrayList;
 
 
@@ -21,14 +27,22 @@ public class Setting_AddDeviceActivity extends AppCompatActivity {
     private ImageView backButton;
     private Button sendButton;
     private EditText deviceAddEdit;
+    public static Context context_main;
     String key;
     ArrayList<String> device_key = new ArrayList<>();
+    ArrayList<String> save_device = new ArrayList<>();
+    ArrayList<String> savedevice = new ArrayList<>();
 
+    private static final String SETTINGS_PLAYER_JSON2 = "settings_item_json2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_device_add);
+
+        context_main = this;
+
+        save_device = getStringArrayPref(getApplicationContext(), SETTINGS_PLAYER_JSON2);
 
         device_key = ((MainActivity)MainActivity.context_main).device_key;
 
@@ -52,16 +66,60 @@ public class Setting_AddDeviceActivity extends AppCompatActivity {
                 if (device_key.contains(key)) {
                     final FirebaseDatabase database = FirebaseDatabase.getInstance();
                     DatabaseReference ref = database.getReference(key + "/state");
+                    DatabaseReference ref2 = database.getReference(key + "/id");
                     ref.setValue(true);
                     Toast.makeText(getApplicationContext(), "디바이스 추가에 성공 했습니다!", Toast.LENGTH_LONG).show();
+                    save_device.add("디바이스 키 : " + key + " state : true");
+                    setStringArrayPref(getApplicationContext(), SETTINGS_PLAYER_JSON2, save_device);
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "인증번호를 다시 확인해 주세요.", Toast.LENGTH_LONG).show();
+                    key = "";
                 }
 
             }
         });
 
     }//end of onCreate
+
+    private void setStringArrayPref(Context context, String key, ArrayList<String> values) {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+
+        editor.apply();
+    }
+
+    private ArrayList getStringArrayPref(Context context, String key) {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = prefs.getString(key, null);
+        ArrayList urls = new ArrayList();
+
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
+    }
 
 }//end of class
--- app/src/main/java/com/example/look_out/Setting_DeviceActivity.java
+++ app/src/main/java/com/example/look_out/Setting_DeviceActivity.java
@@ -1,29 +1,59 @@
 package com.example.look_out;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Setting_DeviceActivity extends AppCompatActivity {
     private ImageView backButton;
     private Button addButton;
     private Button deleteButton;
+    private ListView listView;
+    ArrayList<String> save_device = new ArrayList<>();
+    ArrayList<String> savedevice = new ArrayList<>();
 
+    private static final String SETTINGS_PLAYER_JSON3 = "settings_item_json3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_device);
+        save_device.clear();
+
+
+        save_device = getStringArrayPref(getApplicationContext(), SETTINGS_PLAYER_JSON3);
 
         backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -43,8 +73,77 @@ public class Setting_DeviceActivity extends AppCompatActivity {
             }
         });
 
+        listView = (ListView)findViewById(R.id.listView);
+
+        ArrayAdapter<String> adpater = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, savedevice){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                View view = super.getView(position, convertView, parent);
+                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+                tv.setTextColor(Color.rgb(230,230,230));
+                return view;
+            }
+        };
+
+        try {
+            save_device = ((Setting_AddDeviceActivity) Setting_AddDeviceActivity.context_main).save_device;
+        }
+        catch(Exception e){
+        }
+
+        savedevice.clear();
+        for(int i = 0; i < save_device.size(); i++){
+            if(!savedevice.contains(save_device.get(i))){
+                savedevice.add(save_device.get(i));
+            }
+        }
+
+        setStringArrayPref(getApplicationContext(), SETTINGS_PLAYER_JSON3, savedevice);
+        listView.setAdapter(adpater);
+
+
         deleteButton = findViewById(R.id.deleteButton);
 
     }//end of onCreate
+
+    private void setStringArrayPref(Context context, String key, ArrayList<String> values) {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+
+        editor.apply();
+    }
+
+    private ArrayList getStringArrayPref(Context context, String key) {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = prefs.getString(key, null);
+        ArrayList urls = new ArrayList();
+
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
+    }
 
 }//end of class
