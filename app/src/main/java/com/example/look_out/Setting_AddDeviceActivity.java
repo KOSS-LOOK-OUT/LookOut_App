2ad975e0eab6d44802bff988a24cfac35aaccca4
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2022-08-27 21:41:43 +0900

    feat: 버튼 클릭 시 입력한 값 날아가도록 설정 변경

--- app/src/main/java/com/example/look_out/Setting_AddDeviceActivity.java
+++ app/src/main/java/com/example/look_out/Setting_AddDeviceActivity.java
@@ -70,10 +70,12 @@ public class Setting_AddDeviceActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "디바이스 추가에 성공 했습니다!", Toast.LENGTH_LONG).show();
                     save_device.add(key);
                     setStringArrayPref(getApplicationContext(), SETTINGS_PLAYER_JSON2, save_device);
+                    deviceAddEdit.setText("");
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "인증번호를 다시 확인해 주세요.", Toast.LENGTH_LONG).show();
                     key = "";
+                    deviceAddEdit.setText("");
                 }
 
             }
