d239189f42ab14c267b28e808e58fc295c526dce
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2022-08-29 22:19:57 +0900

    docs: 정한 규칙에 따라 주석 수정

--- app/src/main/java/com/example/look_out/MainActivity.java
+++ app/src/main/java/com/example/look_out/MainActivity.java
@@ -1,24 +1,17 @@
 package com.example.look_out;
 
 /**
- *  @filename MainActivity.java
- *
- *  LookOut 앱이 실행되면 가장 먼저 실행됩니다.
- *  기본 화면이며 가장 많은 Activity들과 상호작용 합니다.
- *  디바이스가 등록되어 있으면 중앙의 로그의 애니메이션이 실행된다.
- *  디바이스가 등록되어 있지 않으면 애니메이션이 실행이 되지 않는다.
- *
- *  사용방법:
- *  설정 아이콘을 누르면 설정창으로 넘어갈 수 있다.
- *  뒤로 가기 버튼을 누르면 첫번째는 토스트메세지가 뜨고 한번 더 누르면 앱을 종료할 수 있다.
- *
- *  @author 김언지
- *  @author 김지윤
- *  @author 김준영
- *  @author 이채영
- *  @version 1.1
+ * @filename MainActivity.java
+ * @author 이채영
+ * @author 김지윤
+ * @author 김언지
+ * @version 1.1
+ * 앱이 실행되면 처음으로 나오는 클래스
+ * 사용 방법:
+ * 이전 버튼을 한 번 누르면 한번 더 이전버튼을 누르면 앱이 종료된다는 메세지가 뜨고 1.5초안에 한 번 더 누르면 앱이 종료된다.
+ * 설정 imageView를 누르면 설정창으로 넘어간다.
+ * 등록해놓은 디바이스가 있다면, 중앙의 로그의 애니메이션이 실행되고 소리를 감지 중이라는 text가 뜨고 없다면, 연결된 디바이스가 없다는 text가 뜬다.
  */
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -52,15 +45,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-/**
- * Android 3.0(API Level 11) 버전부터 지원되는 ActionBar를 해당 버전 이하의 버전에도 적용할 수 있도록 지원해주는 AppCompatActivity를 확장해서 사용
- */
-
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * 다른 Activity에서 MainActivity의 시스템 요소와 자원에 접근하기 위한 변수
-     */
     public static Context context_main;
     private final long finishtimed = 1500;
     private long presstime = 0;
@@ -68,9 +54,25 @@ public class MainActivity extends AppCompatActivity {
     private final int MY_PERMISSIONS_REQUEST = 1000;
     private ImageView iconCircle;
     private TextView statusMessage;
+
+    /**
+     * 감지된 소리의 내용(불이야, 조심해, 도둑이야)
+     */
     String value;
+
+    /**
+     * firebase에 등록된 인증번호와 키 값들의 리스트
+     */
     ArrayList<String> device_key = new ArrayList<>();
+
+    /**
+     * 알림 기록을 저장하기 위한 리스트
+     */
     ArrayList<String> al_log = new ArrayList<>();
+
+    /**
+     * 등록된 디바이스들의 uuid를 저장하기 위한 리스트
+     */
     ArrayList<String> device_uuid = new ArrayList<>();
 
     private static final String SETTINGS_PLAYER_JSON = "settings_item_json";
@@ -80,44 +82,33 @@ public class MainActivity extends AppCompatActivity {
      * 필수 구현 요소
      * Activity가 생성될 때 실행된다.
      * 화면 정의하는 용도로 많이 사용한다.
-     *
      * @param savedInstanceState 비 영구적 동적 데이터
      */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-        /**
-         * super호출을 통해 백그라운드에서 실행할 수 있게 해줌.
-         */
         super.onCreate(savedInstanceState);
-
-
-        /**
-         * MainActivity.java와 activity_main.xml을 연결해줌.
-         */
         setContentView(R.layout.activity_main);
 
         /**
-         * 다른 Activity에서 context변수를 통해 MainActivity에 접근이 가능하게 함.
+         * 다른 Activity에서 context변수를 통해 MainActivity에 접근이 가능하게 한다.
          */
         context_main = this;
 
         /**
-         * SharedPreferences에 키값 SETTINGS_PLAYER_JSON2에 접근해서 Json 형식의 String을 읽어와 다시 ArrayList로 변환해 device_uuid에 저장함.
+         * SharedPreferences에 키값 SETTINGS_PLAYER_JSON2에 접근해서 Json 형식의 String을 읽어와 다시 ArrayList로 변환해 device_uuid에 저장한다.
          */
         device_uuid = getStringArrayPref(getApplicationContext(), SETTINGS_PLAYER_JSON2);
 
         /**
-         * SharedPreferences에 키값 SETTINGS_PLAYER_JSON에 접근해서 Json 형식의 String을 읽어와 다시 ArrayList로 변환해 al_log에 저장함.
+         * SharedPreferences에 키값 SETTINGS_PLAYER_JSON에 접근해서 Json 형식의 String을 읽어와 다시 ArrayList로 변환해 al_log에 저장한다.
          */
         al_log = getStringArrayPref(getApplicationContext(), SETTINGS_PLAYER_JSON);
 
 
         /**
-         * al_log의 크기가 50이 넘는다면 가장 최근에 저장된 50개 빼고 나머지 데이터들을 삭제함.
-         * ArrayList의 데이터(al_log)를 Json형식으로 변환하여 1개의 String으로 만든 후 이를 SharedPreferences에 키 값 SETTINGS_PLAYER_JSON으로 저장함.
+         * al_log의 크기가 50이 넘는다면 가장 최근에 저장된 50개 빼고 나머지 데이터들을 삭제한다.
+         * ArrayList의 데이터(al_log)를 Json형식으로 변환하여 1개의 String으로 만든 후 이를 SharedPreferences에 키 값 SETTINGS_PLAYER_JSON으로 저장한다.
          */
         if(al_log.size() > 50) {
             for (int i = al_log.size() - 51; i >= 0; i--) {
@@ -127,11 +118,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         /**
-         * 데이터베이스에서 데이터를 읽어오기위해 DatabaseReference 인스턴스를 생성
-         * 하위 이벤트를 수신 대기하는 addchildEventlistener 사용
-         * onChildAdded()로 항목 목록을 검색하거나 항목 목록에 대한 추가를 수신 대기
-         * onChildChanged()로 목록의 항목에 대한 변경사항을 수신 대기
-         * 현재 연결되어 있는 firebase에 있는 모든 키값들을 가져와 device_key에 add
+         * 데이터베이스에서 데이터를 읽어오기위해 DatabaseReference 인스턴스를 생성한다.
+         * 하위 이벤트를 수신 대기하는 addchildEventlistener 사용한다.
+         * onChildAdded()로 항목 목록을 검색하거나 항목 목록에 대한 추가를 수신 대기한다.
+         * onChildChanged()로 목록의 항목에 대한 변경사항을 수신 대기한다.
+         * 현재 연결되어 있는 firebase에 있는 모든 키값들을 가져와 device_key에 add한다.
          */
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference();
@@ -164,10 +155,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
         /**
-         * 등록되어 있는 디바이스의 id 값을 device_uuid 리스트를 통해 하나씩 get해와서 firebase에 접근함.
-         * uuid/content에 있는 항목이 변경될 때마다 getValue()를 통해 value값을 가져옴.
-         * 가져온 value값과 동록된 키워드인 "불이야", "도둑이야", "조심해"를 equals를 통해 비교함.
-         * 일치한다면 현재시간과 value값을 로그로 저장하고 AlarmActivity를 실행함.
+         * 등록되어 있는 디바이스의 id 값을 device_uuid 리스트를 통해 하나씩 get해와서 firebase에 접근한다.
+         * uuid/content에 있는 항목이 변경될 때마다 getValue()를 통해 value값을 가져온다.
+         * 가져온 value값과 동록된 키워드인 "불이야", "도둑이야", "조심해"를 equals를 통해 비교한다.
+         * 일치한다면 현재시간과 value값을 로그로 저장하고 AlarmActivity를 실행한다.
          */
         for(int i = 0; i<device_uuid.size(); i++){
             DatabaseReference ref2 = database.getReference(device_uuid.get(i) + "/content");
@@ -227,11 +218,9 @@ public class MainActivity extends AppCompatActivity {
 
         /**
          * 전화 권한, 문자 권한 중 하나라도 허용되지 않았으면 실행
+         * 사용자가 권한 요청을 명시적으로 거부하였는지 권한 요청을 처음 보거나 다시 묻지 묻지 않음으로 선택하였는지 확인한 후 전화 권한, 문자 권한 요청
          */
         if (permissionCall == PackageManager.PERMISSION_DENIED || permissionSMS == PackageManager.PERMISSION_DENIED) {
-            /**
-             * 사용자가 권한 요청을 명시적으로 거부하였는지 권한 요청을 처음 보거나 다시 묻지 묻지 않음으로 선택하였는지 확인한 후 전화 권한, 문자 권한 요청
-             */
             if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CALL_PHONE)|| ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.SEND_SMS)) {
                 ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST);
             } else {
@@ -241,8 +230,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         /**
-         * activity_main.xml파일에서 id가 setting으로 설정된 View를 가져옴.
-         * setting이 눌릴 때, MainActivity에서 SettingActivity로 이동.
+         * activity_main.xml파일에서 id가 setting으로 설정된 View를 가져온다.
+         * setting이 눌릴 때, MainActivity에서 SettingActivity로 이동한다.
          */
         setting = findViewById(R.id.setting);
         setting.setOnClickListener(new View.OnClickListener() {
@@ -254,12 +243,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
         /**
-         * activity_main.xml파일에서 id가 iconCircle과 statusMessage으로 설정된 View를 가져옴.
-         * rotate_anim.xml 불러와 animation을 정의한다
+         * activity_main.xml파일에서 id가 iconCircle과 statusMessage으로 설정된 View를 가져온다.
+         * rotate_anim.xml 불러와 animation을 정의한다.
          * 연결된 디바이스가 없으면 "연결된 디바이스가 없습니다." 문구를 띄윤다.
          * 연결된 디바이스가 한 개라도 있으면 애니메이션을 iconCircle을 돌아가게끔 애니메이션을 set 해주고 "위험한 소리를 감지하고 있습니다.."라는 문구를 띄워준다.
          */
-
         iconCircle = (ImageView)findViewById(R.id.iconCircle);
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_anim);
 
@@ -274,7 +262,7 @@ public class MainActivity extends AppCompatActivity {
     }//end of onCreate
 
     /**
-     * 권한 체크 이후 돌아가는 메서드로 모든 퍼미션을 허용했는지 체크
+     * 권한 체크 이후 돌아가는 메서드로 모든 퍼미션을 허용했는지 체크한다.
      * @param requestCode 퍼미션 요청 코드로 어떤 권한 세트를 지정했는지 확인하기 위한 임의의 상수
      * @param permissions 요청한 권한 세트
      * @param grantResults 요청한 권한 세트의 허용, 거부 결과 세트로 허용, 거부 결과가 담긴 길이
@@ -289,7 +277,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //뒤로가기 두 번 눌러서 종료하기
+    /**
+     * 안드로이드 폰에 내장된 이전버튼을 눌렀을 경우 구조적으로 이전 activity인 창으로 넘어가게 한다.
+     */
     public void onBackPressed () {
         long tempTime = System.currentTimeMillis();
         long intervalTime = tempTime - presstime;
@@ -304,7 +294,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * 알림 로그에 저장하기 위해서 현재 시간을 "yyyy-MM-dd hh:mm:ss"로 표시하는 메소드
+     * 알림 로그에 저장하기 위해서 현재 시간을 "yyyy-MM-dd hh:mm:ss"로 표시하는 메소드이다.
      */
     private String getTime () {
         long now = System.currentTimeMillis();
@@ -317,8 +307,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * ArrayList를 Json으로 변환하여 SharedPreferences에 String을 저장하는 코드
-     *
+     * ArrayList를 Json으로 변환하여 SharedPreferences에 String을 저장한다.
      * @param context 애플리케이션의 현재 상태
      * @param key SharedPreference를 보낼 key 값
      * @param values String 형태로 저장할 ArrayList
@@ -343,8 +332,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * SharedPreferences에서 Json형식의 String을 가져와서 다시 ArrayList로 변환하는 코드
-     *
+     * SharedPreferences에서 Json형식의 String을 가져와서 다시 ArrayList로 변환한다.
      * @param context 애플리케이션의 현재 상태
      * @param key SharedPreference를 받아올 key 값
      */
