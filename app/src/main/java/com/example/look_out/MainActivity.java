eaf4655a1e303d696bc8ee679b0bfb850ee1551b
hummingbbird <pop9814@kookmin.ac.kr>
2022-09-01 20:13:17 +0900

    docs: makePush 주석 추가

--- app/src/main/java/com/example/look_out/MainActivity.java
+++ app/src/main/java/com/example/look_out/MainActivity.java
@@ -274,50 +274,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }//end of onCreate
 
+    /**
+     * 푸쉬 알림을 생성하는 메소드
+     * @param s 키워드값(ex) 불이야, 조심해, 도둑이야)
+     */
     public void makePush(String s) {
 
-        //알림(Notification)을 관리하는 관리자 객체를 운영체제(Context)로부터 소환하기
         NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        //Notification 객체를 생성해주는 건축가객체 생성(AlertDialog 와 비슷)
         NotificationCompat.Builder builder= null;
 
-        //Oreo 버전(API26 버전)이상에서는 알림시에 NotificationChannel 이라는 개념이 필수 구성요소가 됨.
+        /**
+         * Oreo 버전(API26 버전)이상에서는 알림시에 NotificationChannel 이라는 개념이 필수 구성요소가 됨.
+         */
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-
             String channelID="channel_01"; //알림채널 식별자
             String channelName="MyChannel01"; //알림채널의 이름(별명)
 
-            //알림채널 객체 만들기
             NotificationChannel channel= new NotificationChannel(channelID,channelName,NotificationManager.IMPORTANCE_DEFAULT);
-
-            //알림매니저에게 채널 객체의 생성을 요청
             notificationManager.createNotificationChannel(channel);
 
-            //알림건축가 객체 생성
             builder=new NotificationCompat.Builder(this, channelID);
-
-
         }else{
-            //알림 건축가 객체 생성
             builder= new NotificationCompat.Builder(MainActivity.this, (Notification) null);
         }
 
-        //건축가에게 원하는 알림의 설정작업
         builder.setSmallIcon(android.R.drawable.ic_menu_view);
+        builder.setContentTitle("위험 감지");
+        builder.setContentText("\"" + s + "\" 소리가 감지되었습니다!");
 
-        //상태바를 드래그하여 아래로 내리면 보이는
-        //알림창(확장 상태바)의 설정
-        builder.setContentTitle("위험 감지");//알림창 제목
-        builder.setContentText("\"" + s + "\" 소리가 감지되었습니다!");//알림창 내용
-        //알림창의 큰 이미지
         Bitmap bm= BitmapFactory.decodeResource(getResources(),R.drawable.group221);
         builder.setLargeIcon(bm);//매개변수가 Bitmap을 줘야한다.
 
-        //건축가에게 알림 객체 생성하도록
         Notification notification=builder.build();
-
-        //알림매니저에게 알림(Notify) 요청
         notificationManager.notify(1, notification);
 
 
